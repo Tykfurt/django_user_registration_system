b5d8ddd18ccfb54cad444c641d156798d80cb059
John Florez_Otalvaro <johvo93@gmail.com>
2026-04-22 19:13:40 -0500

    Add files via upload

---
+++ register/setting.py
@@ -0,0 +1,18 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'nombre_app',
+]
+    import os
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # important
+        'APP_DIRS': True,
+    },
+]
